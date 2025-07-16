35cbd291cf8251633ea7c6e4d0bbd65d9d22d6a3
Minwoo <jungmw@eseict.com>
2025-07-16 17:25:53 +0900

    [Feat] Xml Plugins
    1. Xml Plugins 추가
     - Path 에 맞는 xml파일을 생성하는 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// XML 응답을 위한 커스텀 플러그인
+function xmlResponsePlugin() {
+  return {
+    name: 'xml-response',
+    configureServer(server: any) {
+      server.middlewares.use('/', (req: any, res: any, next: any) => {
+        const url = req.url || ''
+        
+        // /{name}.xml 패턴 매칭
+        const xmlMatch = url.match(/^\/([^\/]+)\.xml$/)
+        
+        if (xmlMatch) {
+          const name = xmlMatch[1]
+          const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+<config>
+  ${name}
+</config>`
+          
+          res.setHeader('Content-Type', 'application/xml; charset=utf-8')
+          res.setHeader('Cache-Control', 'no-cache')
+          res.end(xmlContent)
+          return
+        }
+        
+        next()
+      })
+    }
+  }
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), xmlResponsePlugin()],
 })
